e6f659f96579417cb26e959c55fb8c98f171c5e2
Chazza <github@whalecoiner.com>
2022-05-15 22:04:13 +0200

    libvirt terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,16 @@
 terraform {
   required_providers {
-    proxmox = {
-      source = "telmate/proxmox"
-      version = "2.9.10"
+    libvirt = {
+      source = "dmacvicar/libvirt"
+      version = "0.6.14"
     }
   }
 }
 
-provider "proxmox" {
-  # Configuration options
-  pm_api_url = "https://192.168.1.116:8006/api2/json"
-  # Run the following
-  # eval $(op signin) 
-  # op run --env-file=.env -- terraform apply
+provider "libvirt" {
+   uri = "qemu+ssh://ubuntu@192.168.1.116/system"
 }
 
-resource "proxmox_vm_qemu" "resource-name" {
-  name        = "created-via-tf"
-  target_node = "pve"
-  iso         = "ubuntu-22.04-live-server-amd64.iso"
-
-  ### or for a Clone VM operation
-  # clone = "template to clone"
-
-  ### or for a PXE boot VM operation
-  # pxe = true
-  # boot = "net0;scsi0"
-  # agent = 0
+resource "libvirt_domain" "terraform_test" {
+  name = "terraform_test"
 }
