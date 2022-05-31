2b9a0cb424120d424ebbc4edc5ba2a46a750a001
Chazza <github@whalecoiner.com>
2022-05-31 07:23:00 +0200

    debug

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ resource "libvirt_cloudinit_disk" "commoninit" {
   name           = "commoninit.iso"
   user_data      = data.template_file.user_data.rendered
   network_config = data.template_file.network_config.rendered
-  pool           = libvirt_pool.ubuntu.name
+  pool           = libvirt_pool.ubuntu2.name
 }
 
 resource "libvirt_domain" "domain-ubuntu" {
