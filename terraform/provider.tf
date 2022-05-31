cfce9855d5eff3ead78d3cfe825954abe99418f8
Chazza <github@whalecoiner.com>
2022-05-31 12:30:43 +0200

    debug

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    mycloud = {
+      source  = "dmacvicar/libvirt"
+      version = "~> 1.0"
+    }
+  }
+}
+
 provider "libvirt" {
    uri = "qemu+ssh://ubuntu@192.168.1.116/system"
 }
