927596e1178dbd65a1fa6550fa404a6cebf0435b
Chazza <github@whalecoiner.com>
2022-05-31 07:16:57 +0200

    debug

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,25 +2,93 @@ provider "libvirt" {
    uri = "qemu+ssh://ubuntu@192.168.1.116/system"
 }
 
-resource "libvirt_pool" "pool" {
-  name = "default-pool"
+resource "libvirt_pool" "ubuntu" {
+  name = "ubuntu"
   type = "dir"
-  path = "/tank/terraform-provider-libvirt-pool-default"
+  path = var.libvirt_disk_path
 }
 
-resource "libvirt_volume" "server_data_disk" {
-  name  = "server-data-disk"
-  size  = 32
-  pool  = pool.id
+resource "libvirt_volume" "ubuntu-qcow2" {
+  name = "ubuntu-qcow2"
+  pool = libvirt_pool.ubuntu.name
+  source = var.ubuntu_18_img_url
+  format = "qcow2"
 }
 
-# Create the machine
+data "template_file" "user_data" {
+  template = file("${path.module}/config/cloud_init.yml")
+}
+
+data "template_file" "network_config" {
+  template = file("${path.module}/config/network_config.yml")
+}
+
+resource "libvirt_cloudinit_disk" "commoninit" {
+  name           = "commoninit.iso"
+  user_data      = data.template_file.user_data.rendered
+  network_config = data.template_file.network_config.rendered
+  pool           = libvirt_pool.ubuntu.name
+}
+
 resource "libvirt_domain" "domain-ubuntu" {
-  name   = "ubuntu-terraform"
+  name   = var.vm_hostname
   memory = "512"
   vcpu   = 1
-}
 
-terraform {
-  required_version = ">= 0.12"
+  cloudinit = libvirt_cloudinit_disk.commoninit.id
+
+  network_interface {
+    network_name   = "default"
+    wait_for_lease = true
+    hostname       = var.vm_hostname
+  }
+
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "1"
+  }
+
+  disk {
+    volume_id = libvirt_volume.ubuntu-qcow2.id
+  }
+
+  graphics {
+    type        = "spice"
+    listen_type = "address"
+    autoport    = true
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Hello World'"
+    ]
+
+    connection {
+      type                = "ssh"
+      user                = var.ssh_username
+      host                = libvirt_domain.domain-ubuntu.network_interface[0].addresses[0]
+      private_key         = file(var.ssh_private_key)
+      bastion_host        = "ams-kvm-remote-host"
+      bastion_user        = "deploys"
+      bastion_private_key = file("~/.ssh/deploys.pem")
+      timeout             = "2m"
+    }
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "[nginx]" > nginx.ini
+      echo "${libvirt_domain.domain-ubuntu.network_interface[0].addresses[0]}" >> nginx.ini
+      echo "[nginx:vars]" >> nginx.ini
+      echo "ansible_ssh_common_args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ProxyCommand=\"ssh -W %h:%p -q ams-kvm-remote-host\"'" >> nginx.ini
+      ansible-playbook -u ${var.ssh_username} --private-key ${var.ssh_private_key} -i nginx.ini ansible/playbook.yml
+      EOT
+  }
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "libvirt_disk_path" {
+  description = "path for libvirt pool"
+  default     = "/opt/kvm/pool1"
+}
+
+variable "ubuntu_18_img_url" {
+  description = "ubuntu 18.04 image"
+  default     = "http://cloud-images.ubuntu.com/releases/bionic/release-20191008/ubuntu-18.04-server-cloudimg-amd64.img"
+}
+
+variable "vm_hostname" {
+  description = "vm hostname"
+  default     = "terraform-kvm-ansible"
+}
+
+variable "ssh_username" {
+  description = "the ssh user to use"
+  default     = "ubuntu"
+}
+
+variable "ssh_private_key" {
+  description = "the private key to use"
+  default     = "~/.ssh/id_rsa"
+}
