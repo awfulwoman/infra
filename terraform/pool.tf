2db658107a95a2f3439a559326f9da69fa496dd7
Chazza <github@whalecoiner.com>
2022-05-31 12:42:07 +0200

    WIP

--- terraform/pool.tf
+++ terraform/pool.tf
@@ -1,5 +1,5 @@
 resource "libvirt_pool" "k8s-pool" {
   name = "k8s-pool"
   type = "dir"
-  path = "/tank/images"
+  path = "/tank/images/k8s-pool"
 }
--- terraform/volumes.tf
+++ terraform/volumes.tf
@@ -1,6 +1,6 @@
-resource "libvirt_volume" "base" {
-  name = "base"
-  pool = "k8s-pool"
-  source = "/tank/images/ubuntu-22.04-minimal-cloudimg-amd64.qcow2"
-  format = "qcow2"
-}
+# resource "libvirt_volume" "base" {
+#   name = "base"
+#   pool = "k8s-pool"
+#   source = "/tank/images/k8s-pool/ubuntu-22.04-minimal-cloudimg-amd64.qcow2"
+#   format = "qcow2"
+# }
