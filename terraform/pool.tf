5d2e855dc60e5acd5f8fe9af92d9544cd8401164
Chazza <github@whalecoiner.com>
2022-05-31 14:01:39 +0200

    WIP

--- terraform/pool.tf
+++ terraform/pool.tf
@@ -1,5 +1,14 @@
+# This is the same as the following command:
+# virsh pool-define-as default --type dir --target /tank/pools/default
+# https://unix.stackexchange.com/questions/216837/virsh-and-creating-storage-pools-what-is-sourcepath
+# It creates a directory and assigns it to the libvirt pool named "default"
+
 resource "libvirt_pool" "default" {
   name = "default"
   type = "dir"
   path = "/tank/pools/default"
 }
+
+# pool-define-as --name default --source-name filepool --type zfs
+# It takes a ZFS pool and assigns it in totality to the libvirt pool named "default"
+# The Terraform provider does not yet support ZFS pools so this would have to be done manually.
--- terraform/volumes.tf
+++ terraform/volumes.tf
@@ -9,3 +9,10 @@ resource "libvirt_volume" "opensuse_leap" {
   name   = "opensuse_leap"
   source = "http://download.opensuse.org/repositories/Cloud:/Images:/Leap_42.1/images/openSUSE-Leap-42.1-OpenStack.x86_64.qcow2"
 }
+
+# Attach to all VMs in the terraform_test domain
+
+resource "libvirt_volume" "terraform_test" {
+  name           = "master.qcow2"
+  base_volume_id = libvirt_volume.opensuse_leap.id
+}
