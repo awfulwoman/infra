8bc519cf16c8b37e49cf2512633b193dd6980de4
Chazza <github@whalecoiner.com>
2022-05-31 07:28:07 +0200

    debug

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,67 +1,6 @@
 provider "libvirt" {
    uri = "qemu+ssh://ubuntu@192.168.1.116/system"
 }
-
-resource "libvirt_pool" "ubuntu2" {
-  name = "ubuntu"
-  type = "dir"
-  path = var.libvirt_disk_path
-}
-
-resource "libvirt_volume" "ubuntu-qcow2" {
-  name = "ubuntu-qcow2"
-  pool = libvirt_pool.ubuntu2.name
-  source = var.ubuntu_18_img_url
-  format = "qcow2"
-}
-
-data "template_file" "user_data" {
-  template = file("${path.module}/config/cloud_init.yaml")
-}
-
-data "template_file" "network_config" {
-  template = file("${path.module}/config/network_config.yaml")
-}
-
-resource "libvirt_cloudinit_disk" "commoninit" {
-  name           = "xcommoninit.iso"
-  user_data      = data.template_file.user_data.rendered
-  network_config = data.template_file.network_config.rendered
-  pool           = libvirt_pool.ubuntu2.name
-}
-
-resource "libvirt_domain" "domain-ubuntu" {
-  name   = var.vm_hostname
-  memory = "512"
-  vcpu   = 1
-
-  cloudinit = libvirt_cloudinit_disk.commoninit.id
-
-  network_interface {
-    network_name   = "default"
-    wait_for_lease = true
-    hostname       = var.vm_hostname
-  }
-
-  console {
-    type        = "pty"
-    target_port = "0"
-    target_type = "serial"
-  }
-
-  console {
-    type        = "pty"
-    target_type = "virtio"
-    target_port = "1"
-  }
-
-  disk {
-    volume_id = libvirt_volume.ubuntu-qcow2.id
-  }
-
-  graphics {
-    type        = "spice"
-    listen_type = "address"
-    autoport    = true
-  }
+resource "libvirt_domain" "terraform_test" {
+  name = "terraform_test"
 }
