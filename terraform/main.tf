b4477de8bc0aef8ff09c04cdfcbb20c7eec2259d
Chazza <github@whalecoiner.com>
2022-05-31 14:04:25 +0200

    WIP

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,6 @@
 
 resource "libvirt_domain" "terraform_test" {
   name = "terraform_test"
+  memory = "1024"
+  vcpu   = 1
 }
--- terraform/volumes.tf
+++ terraform/volumes.tf
@@ -6,7 +6,8 @@
 # }
 
 resource "libvirt_volume" "opensuse_leap" {
-  name   = "opensuse_leap"
+  name = "opensuse_leap"
+  pool = "default"
   source = "http://download.opensuse.org/repositories/Cloud:/Images:/Leap_42.1/images/openSUSE-Leap-42.1-OpenStack.x86_64.qcow2"
 }
 
