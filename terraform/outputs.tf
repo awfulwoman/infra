37b9bb39a8b2dc16577364b8f88465f8b92341cf
Chazza <github@whalecoiner.com>
2022-05-31 07:29:14 +0200

    deug tf

--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "ip" {
-  value = libvirt_domain.domain-ubuntu.network_interface[0].addresses[0]
-}
-
-output "url" {
-  value = "http://${libvirt_domain.domain-ubuntu.network_interface[0].addresses[0]}"
-}
