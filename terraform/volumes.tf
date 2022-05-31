4f327910310c5fcab268c08fd2d5cbcb3301a71f
Chazza <github@whalecoiner.com>
2022-05-31 13:03:14 +0200

    WIP

--- terraform/volumes.tf
+++ terraform/volumes.tf
@@ -4,3 +4,8 @@
 #   source = "/tank/images/k8s-pool/ubuntu-22.04-minimal-cloudimg-amd64.qcow2"
 #   format = "qcow2"
 # }
+
+resource "libvirt_volume" "opensuse_leap" {
+  name   = "opensuse_leap"
+  source = "http://download.opensuse.org/repositories/Cloud:/Images:/Leap_42.1/images/openSUSE-Leap-42.1-OpenStack.x86_64.qcow2"
+}
