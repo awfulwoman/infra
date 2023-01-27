04d43503c3ff6b511899d7e7f9cb67d30fe3a725
Chazza <github@whalecoiner.com>
2023-01-27 17:12:01 +0100

    Terraform ip addresses

--- terraform/digitalocean/main.tf
+++ terraform/digitalocean/main.tf
@@ -29,9 +29,19 @@ output "domain_output" {
   value = data.digitalocean_domain.affordablepotatoes.zone_file
 }
 
+resource "digitalocean_domain" "homeassistant" {
+  name       = "homeassistant.affordablepotatoes.com"
+  ip_address = "192.168.1.103"
+}
+
+resource "digitalocean_domain" "jellyfin" {
+  name       = "jellyfin.affordablepotatoes.com"
+  ip_address = "192.168.1.116"
+}
+
 resource "digitalocean_droplet" "host_public" {
   image  = "ubuntu-22-10-x64"
   name   = "host-public"
-  region = "fra"
+  region = "fra1"
   size   = "s-1vcpu-1gb"
 }
