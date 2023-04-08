56f765cd66a1905022d5ee0bad8c85a3d6201046
Chazza <github@whalecoiner.com>
2023-04-08 10:42:58 +0200

    git -c

--- bootstrap-ansible-meta.sh
+++ bootstrap-ansible-meta.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # /opt/ansible/home is standard install location on hosts
 
-cd /opt/ansible/home/
-git pull
+git -C /opt/ansible/home/ pull
 ansible-galaxy install -r /opt/ansible/home/ansible/meta/requirements.yaml -p /opt/ansible/galaxy-roles
 ansible-galaxy collection install -r /opt/ansible/home/ansible/meta/requirements.yaml -p /opt/ansible/galaxy-roles
