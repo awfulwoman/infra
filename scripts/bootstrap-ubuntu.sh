50c05c41e3f4ec0c75e20b00f15665119cc33cbe
Chazza <github@whalecoiner.com>
2023-04-08 12:39:27 +0200

    Add more messages

--- scripts/bootstrap-ubuntu.sh
+++ scripts/bootstrap-ubuntu.sh
@@ -3,10 +3,24 @@
 # Install Ansible
 which ansible
 if [[ $? != 0 ]] ; then
+	echo " "
+	echo "INSTALL ANSIBLE"
+	echo "************************************"
   python3 -m pip install ansible
 fi
 
 # Satisfy Ansible role dependencies
+echo " "
+echo "UPDATE GIT"
+echo "************************************"
 git -C /opt/ansible/home/ pull
+
+echo " "
+echo "UPDATE ANSIBLE GALAXY ROLES"
+echo "************************************"
 ansible-galaxy install -r /opt/ansible/home/ansible/meta/requirements.yaml -p /opt/ansible/galaxy-roles
+
+echo " "
+echo "UPDATE ANSIBLE GALAXY COLLECTIONS"
+echo "************************************"
 ansible-galaxy collection install -r /opt/ansible/home/ansible/meta/requirements.yaml -p /opt/ansible/collections
