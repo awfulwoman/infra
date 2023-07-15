3a3c5faf274ca27c0abc86af533d132b97d6a491
Chazza <github@whalecoiner.com>
2023-07-15 13:01:15 +0200

    use correct terraform records

--- terraform/affordablepotatoes-digitalocean/main.tf
+++ terraform/affordablepotatoes-digitalocean/main.tf
@@ -20,61 +20,101 @@ provider "digitalocean" {
   token = var.digitalocean_token
 }
 
-data "digitalocean_domain" "affordablepotatoes" {
-  name = "affordablepotatoes.com"
+resource "digitalocean_domain" "default" {
+   name = "affordablepotatoes.com"
 }
 
-output "domain_output" {
-  value = data.digitalocean_domain.affordablepotatoes.zone_file
-}
+
+# output "domain_output" {
+#   value = data.digitalocean_domain.default.zone_file
+# }
+# Nameservers
+# resource "digitalocean_record" "ns1" {
+#   domain = digitalocean_domain.default.id
+#   type   = "NS"
+#   value   = "ns1.digitalocean.com."
+#   name  = "@"
+# }
+
+# resource "digitalocean_record" "ns2" {
+#   domain = digitalocean_domain.default.id
+#   type   = "NS"
+#   value   = "ns2.digitalocean.com."
+#   name  = "@"
+# }
+
+# resource "digitalocean_record" "ns3" {
+#   domain = digitalocean_domain.default.id
+#   type   = "NS"
+#   value   = "ns3.digitalocean.com."
+#   name  = "@"
+# }
+
 
 # Home Automation
-resource "digitalocean_domain" "homeassistant" {
-  name       = "homeassistant.affordablepotatoes.com"
-  ip_address = "100.89.157.31"
+resource "digitalocean_record" "homeassistant" {
+  domain = digitalocean_domain.default.id
+  type   = "A"
+  name   = "homeassistant"
+  value  = "100.89.157.31"
 }
 
-resource "digitalocean_domain" "homeautomation" {
-  name       = "homeautomation.affordablepotatoes.com"
-  ip_address = "100.89.157.31"
+resource "digitalocean_record" "homeautomation" {
+  domain = digitalocean_domain.default.id
+  type   = "A"
+  name   = "homeautomation"
+  value  = "100.89.157.31"
 }
 
-resource "digitalocean_domain" "esphome" {
-  name       = "esphome.affordablepotatoes.com"
-  ip_address = "100.89.157.31"
+resource "digitalocean_record" "esphome" {
+  domain = digitalocean_domain.default.id
+  type   = "A"
+  name   = "esphome"
+  value  = "100.89.157.31"
 }
 
-resource "digitalocean_domain" "zigbee2mqtt" {
-  name       = "zigbee2mqtt.affordablepotatoes.com"
-  ip_address = "100.89.157.31"
+resource "digitalocean_record" "zigbee2mqtt" {
+  domain = digitalocean_domain.default.id
+  type   = "A"
+  name   = "zigbee2mqtt"
+  value  = "100.89.157.31"
 }
 
-resource "digitalocean_domain" "zigbee2mqtt-aqara" {
-  name       = "zigbee2mqtt-aqara.affordablepotatoes.com"
-  ip_address = "100.89.157.31"
+resource "digitalocean_record" "zigbee2mqtt-aqara" {
+  domain = digitalocean_domain.default.id
+  type   = "A"
+  name   = "zigbee2mqtt-aqara"
+  value  = "100.89.157.31"
 }
 
-resource "digitalocean_domain" "uptimekuma" {
-  name       = "uptimekuma.affordablepotatoes.com"
-  ip_address = "100.89.157.31"
+resource "digitalocean_record" "uptimekuma" {
+  domain = digitalocean_domain.default.id
+  type   = "A"
+  name   = "uptimekuma"
+  value  = "100.89.157.31"
 }
-
 
 
 # Storage
-resource "digitalocean_domain" "jellyfin" {
-  name       = "jellyfin.affordablepotatoes.com"
-  ip_address = "100.83.127.33"
+resource "digitalocean_record" "jellyfin" {
+  domain = digitalocean_domain.default.id
+  type   = "A"
+  name   = "jellyfin"
+  value  = "100.83.127.33"
 }
 
-resource "digitalocean_domain" "transmission" {
-  name       = "transmission.affordablepotatoes.com"
-  ip_address = "100.83.127.33"
+resource "digitalocean_record" "transmission" {
+  domain = digitalocean_domain.default.id
+  type   = "A"
+  name   = "transmission"
+  value  = "100.83.127.33"
 }
 
-resource "digitalocean_domain" "gluetun" {
-  name       = "gluetun.affordablepotatoes.com"
-  ip_address = "100.83.127.33"
+resource "digitalocean_record" "gluetun" {
+  domain = digitalocean_domain.default.id
+  type   = "A"
+  name   = "gluetun"
+  value  = "100.83.127.33"
 }
 
 # resource "digitalocean_droplet" "host_public" {
