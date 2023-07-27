1e1e9131536f64aadcf4fdf280761e30643a26e1
Chazza <github@whalecoiner.com>
2023-07-27 14:22:34 +0200

    captailise dammit

--- scripts/bootstrap-ubuntu.sh
+++ scripts/bootstrap-ubuntu.sh
@@ -87,12 +87,12 @@ echo " "
 echo "UPDATE ANSIBLE GALAXY ROLES"
 echo "************************************"
 sudo mkdir -p $ANSIBLE_ROLES_PATH
-sudo chown -r ubuntu:ubuntu $ANSIBLE_ROLES_PATH
+sudo chown -R ubuntu:ubuntu $ANSIBLE_ROLES_PATH
 ansible-galaxy install -r $HOME_REPO_DIR/ansible/meta/requirements.yaml -p $ANSIBLE_ROLES_PATH
 
 echo " "
 echo "UPDATE ANSIBLE GALAXY COLLECTIONS"
 echo "************************************"
 sudo mkdir -p $ANSIBLE_COLLECTIONS_PATH
-sudo chown -r ubuntu:ubuntu $ANSIBLE_COLLECTIONS_PATH
+sudo chown -R ubuntu:ubuntu $ANSIBLE_COLLECTIONS_PATH
 ansible-galaxy collection install -r $HOME_REPO_DIR/ansible/meta/requirements.yaml -p $ANSIBLE_COLLECTIONS_PATH
