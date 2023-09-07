09d4876348ac3d7d52cbc122111a58f1149c9c60
Chazza <github@whalecoiner.com>
2023-09-07 11:33:11 +0200

    add gitea container

--- terraform/affordablepotatoes-digitalocean/main.tf
+++ terraform/affordablepotatoes-digitalocean/main.tf
@@ -201,6 +201,13 @@ resource "digitalocean_record" "bookmarks" {
   value  = "100.83.127.33"
 }
 
+resource "digitalocean_record" "gitea" {
+  domain = digitalocean_domain.default.id
+  type   = "A"
+  name   = "gitea"
+  value  = "100.83.127.33"
+}
+
 # resource "digitalocean_droplet" "host_public" {
 #   image  = "ubuntu-22-10-x64"
 #   name   = "host-public"
