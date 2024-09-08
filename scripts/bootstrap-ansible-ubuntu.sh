b49bdfd23a3dca49aab178732df561bd84bce8ef
Charlie O'Hara <github@whalecoiner.com>
2024-09-08 16:41:23 +0200

    rename

--- scripts/bootstrap-ansible-ubuntu.sh
+++ scripts/bootstrap-ansible-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 BOOTSTRAP_USER_ID=$(id -un)
 BOOTSTRAP_GROUP_ID=$(id -gn)
-ANSIBLEPULL_REPO_URL=https://github.com/whalecoiner/home.git
+ANSIBLEPULL_REPO_URL=https://github.com/awfulwoman/home.git
 # ANSIBLE_VAULT_PASSWORD=""
 
 read -sp "Playbook: " ANSIBLEPULL_PLAYBOOK
