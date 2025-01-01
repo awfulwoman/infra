d839b862ca48941ad1573b0b4796196bd5c796c9
Charlie O'Hara <github@whalecoiner.com>
2025-01-01 18:55:56 +0100

    linux2mqtt service

---
+++ ansible/roles/install-linux2mqtt/templates/linux2mqtt.py
@@ -0,0 +1,14 @@
+from linux2mqtt import Linux2Mqtt, DEFAULT_CONFIG
+
+cfg = Linux2MqttConfig({ 
+  **DEFAULT_CONFIG,
+  "host": "mqtt.{{ domain_name }}",
+})
+
+try:
+  linux2mqtt = Linux2Mqtt(cfg)
+  linux2mqtt.connect()
+  linux2mqtt.loop_busy()
+
+except Exception as ex:
+  # Do something
