76a99901ac6f2bf5bd5cbeaba648b09a687cf4d1
Charlie O'Hara <github@whalecoiner.com>
2025-08-24 21:59:51 +0200

    sleepuntil

--- ansible/roles/ansible-pull/templates/scripts/ansible-pull-compositions.sh
+++ ansible/roles/ansible-pull/templates/scripts/ansible-pull-compositions.sh
@@ -15,23 +15,12 @@ exlock_now || exit 1
 
 ### END Lockfile ###
 
-echo " "
-echo "UPDATE GIT"
-echo "************************************"
-git -C {{ ansiblepull_workdir }}/home/ pull
+# echo " "
+# echo "UPDATE GIT"
+# echo "************************************"
+# git -C {{ ansiblepull_workdir }}/infra/ pull
 
 echo " "
 echo "RUN ANSIBLE PLAYBOOKS"
 echo "************************************"
 ansible-pull -U {{ ansiblepull_repo_url }} ansible/playbooks/{{ host_type }}/{{ inventory_hostname }}/compositions.yaml
-
-if [ $? -eq 0 ]
-then
-  echo "ansible-pull success"
-	# Tell healthchecks.io that all is okay
-  /usr/bin/curl -fsSL https://hc-ping.com/{{ vault_autorestic_ping_key }}/{{ inventory_hostname }}-ansible-pull-compositions > /dev/null
-else
-  echo "ansible-pull-compositions failure"
-	# Report error via Pushover
-	# /usr/bin/curl -s --form-string token="{{ vault_pushover_home_automation_key }}" --form-string user="{{ vault_pushover_user_key }}" --form-string message="{{ inventory_hostname }} ansible-pull failed - $(date --iso-8601=seconds)" https://api.pushover.net/1/messages.json > /dev/null
-fi
