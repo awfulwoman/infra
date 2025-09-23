0fd123c1a5613641820df3a7784e0c5d4363f161
Charlie O'Hara <github@whalecoiner.com>
2025-09-23 22:41:18 +0200

    Improve share site

---
+++ ansible/roles/script-share-personal-site/templates/share-site.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ ! -d "{{ script_share_personal_site_path }}/{{ script_share_personal_site_name }}/.git" ];
+then
+  echo "Cloning"
+  git clone https://gitea.{{ domain_name }}/awfulwoman/personal-site {{ script_share_personal_site_path }}/{{ script_share_personal_site_name }}
+else
+  echo "Updating"
+  cd {{ script_share_personal_site_path }}/{{ script_share_personal_site_name }}
+  git checkout .
+  git clean -f
+  git pull
+fi
+
+echo "Clean up"
+rm -rf {{ script_share_personal_site_path }}/{{ script_share_personal_site_name }}/public
+rm -rf {{ script_share_personal_site_path }}/{{ script_share_personal_site_name }}/resources
+rm -rf {{ script_share_personal_site_path }}/{{ script_share_personal_site_name }}/content
+rm -rf {{ script_share_personal_site_path }}/{{ script_share_personal_site_name }}/.git
+
+echo "Copy content"
+mkdir {{ script_share_personal_site_path }}/{{ script_share_personal_site_name }}/content
+cp -r "{{ script_share_personal_site_path }}/dummycontent/" {{ script_share_personal_site_path }}/{{ script_share_personal_site_name }}/content
+
+# ls -alh "{{ script_share_personal_site_path }}/dummycontent/*" {{ script_share_personal_site_path }}/{{ script_share_personal_site_name }}/content
+
+cd {{ script_share_personal_site_path }}/{{ script_share_personal_site_name }}
+
+# ls -alh .
+
+git config --global init.defaultBranch main
+git init .
+git config user.email "github@whalecoiner.com"
+git config user.name "Charlie O'Hara"
+
+echo "Gitting"
+git add .
+git commit -m "Auto Commit"
+git remote add origin https://{{ vault_github_token_share_site }}@github.com/awfulwoman/personal-site
+git push --set-upstream origin main --force
