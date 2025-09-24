824aa823c534bead01a09f41078d5e11adfc5435
Charlie O'Hara <github@whalecoiner.com>
2025-09-24 16:30:46 +0200

    tidy

--- ansible/roles/script-share-personal-site/templates/share-site.sh
+++ ansible/roles/script-share-personal-site/templates/share-site.sh
@@ -2,38 +2,28 @@
 
 if [ ! -d "{{ script_share_personal_site_path }}/{{ script_share_personal_site_name }}/.git" ];
 then
-  echo "Cloning"
   git clone https://gitea.{{ domain_name }}/awfulwoman/personal-site {{ script_share_personal_site_path }}/{{ script_share_personal_site_name }}
 else
-  echo "Updating"
   cd {{ script_share_personal_site_path }}/{{ script_share_personal_site_name }}
   git checkout .
   git clean -f
   git pull
 fi
 
-echo "Clean up"
 rm -rf {{ script_share_personal_site_path }}/{{ script_share_personal_site_name }}/public
 rm -rf {{ script_share_personal_site_path }}/{{ script_share_personal_site_name }}/resources
 rm -rf {{ script_share_personal_site_path }}/{{ script_share_personal_site_name }}/content
 rm -rf {{ script_share_personal_site_path }}/{{ script_share_personal_site_name }}/.git
 
-echo "Copy content"
 mkdir {{ script_share_personal_site_path }}/{{ script_share_personal_site_name }}/content
 rsync -a "{{ script_share_personal_site_path }}/dummycontent/" {{ script_share_personal_site_path }}/{{ script_share_personal_site_name }}/content
 
-# ls -alh "{{ script_share_personal_site_path }}/dummycontent/*" {{ script_share_personal_site_path }}/{{ script_share_personal_site_name }}/content
-
 cd {{ script_share_personal_site_path }}/{{ script_share_personal_site_name }}
-
-# ls -alh .
-
 git config --global init.defaultBranch main
 git init .
 git config user.email "github@{{ vault_olddomain_wc }}"
 git config user.name "Charlie O'Hara"
 
-echo "Gitting"
 git add .
 git commit -m "Auto Commit $(date +'%Y-%m-%dT%H:%M:%S%z')"
 git remote add origin https://{{ vault_github_token_share_site }}@github.com/awfulwoman/personal-site
