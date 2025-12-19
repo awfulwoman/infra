71d249381bb2c22b9b22f4f828616ad39eb063f1
Charlie O'Hara <github@whalecoiner.com>
2025-12-19 20:41:25 +0100

    tidy zfs script

--- ansible/roles/server-zbackups/templates/pull-zfs-backups.py
+++ ansible/roles/server-zbackups/templates/pull-zfs-backups.py
@@ -5,46 +5,63 @@ import sys
 import argparse
 
 DEFAULT_destination = "backuppool"
+DEFAULT_mode = "push"
 
-def pull_from_host(host, user, dataset, destination):
+def preflight(host, user, datasets, destination, mode):
+    # Check ability to connect to remote host
+    
+    # Check for source
+    
+    # Check for destination
+    
+    # All passed - start backup
+    sendreceive_init(host, user, datasets, destination, mode)
+
+def sendreceive(host, user, dataset, destination, mode):
     try:
-        
         # Find the latest snapshot
-        zfs_find_latest_snapshot=  f"ssh {host} zfs list -t snapshot -H -o name -S creation -r tank | head -n 1"
-
+        # zfs_find_latest_snapshot = f"ssh {host} zfs list -t snapshot -H -o name -S creation -r {dataset} | head -n 1"
+        zfs_find_latest_snapshot = "test"    
 
         # Construct the ZFS send and receive command
-        zfscommand = f"ssh {host} zfs send {user}@{dataset}@{snapshot} | zfs receive {destination}/{host}/{dataset}"
-        
+        if mode == 'push':    
+            zfscommand = f"zfs send {user}@{dataset}@{zfs_find_latest_snapshot} | ssh {host} zfs receive {destination}/{host}/{dataset}"
+        elif mode == 'pull':
+            zfscommand = f"ssh {host} zfs send {user}@{dataset}@{zfs_find_latest_snapshot} | zfs receive {destination}/{host}/{dataset}"
+        else:
+            print("Invalid mode specified", file=sys.stderr)
+            sys.exit(1)
+
+
         # Execute the command
         # subprocess.check_call(zfscommand, shell=True)
-        print(f"SIMULATED OUTPUT: {zfscommand}")
-
-        print(f"Backup from {host}:{dataset} to {destination} completed successfully.")
+        print(f"{zfscommand}")
     except subprocess.CalledProcessError as e:
         print(f"Error during backup of {host}:{dataset}: {e}", file=sys.stderr)
 
 
-def pull_multiple_datasets(user, host, datasets, destination):
+def sendreceive_init(user, host, datasets, destination, mode):
     # print(f"Looping over datasets {datasets}")
     for dataset in datasets:
-        pull_from_host(host, user, dataset, destination)
+        # print(f"Backing up {dataset}")
+        sendreceive(host, user, dataset, destination, mode)
 
 if __name__ == "__main__":
     # if len(sys.argv) < 3:
     #     print("Usage: pull-zfs-backups.py --user <user> --host <host> --datasets <datasets> [<destination>]", file=sys.stderr)
     #     sys.exit(1)
 
-    parser = argparse.ArgumentParser(description='Pull ZFS backups from a remote host.')
-    parser.add_argument('--user', help='Remote user for SSH')
-    parser.add_argument('--host', help='Remote host to pull from')
-    parser.add_argument('--datasets', nargs='+', help='Remote datasets to pull')
-    parser.add_argument('--destination', default=DEFAULT_destination, help='Local pool to receive backups (default: %(default)s)')
+    parser = argparse.ArgumentParser(description='Push or pull ZFS backups from a remote host')
+    parser.add_argument('--user', help='Remote SSH user')
+    parser.add_argument('--host', help='Remote host')
+    parser.add_argument('--datasets', nargs='+', help='Source datasets')
+    parser.add_argument('--destination', default=DEFAULT_destination, help='Destination dataset to receive backups (default: %(default)s)')
+    parser.add_argument('--mode', default=DEFAULT_mode, help='Either push or pull from server (default: %(default)s)')
 
     args = parser.parse_args()
 
     if not args.user or not args.host or not args.datasets:
-        print("Usage: pull-zfs-backups.py --user <user> --host <host> --datasets <space-seperated list> [--datasets <destination>]", file=sys.stderr)
+        print("Usage: pull-zfs-backups.py --user <user> --host <host> --datasets <space-seperated list> [--datasets <destination> --mode <mode>]", file=sys.stderr)
         sys.exit(1)
 
-    pull_multiple_datasets(args.user, args.host, args.datasets, args.destination)
+    preflight(args.user, args.host, args.datasets, args.destination, args.mode)
